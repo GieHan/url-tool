0cb0eeb3b7c0f1a1b6952ad3bccd03f75a17f78c
GieHan <Gie.Hanggoro@gmail.com>
2022-04-18 10:37:14 +0200

    Create empty spring boot project

---
+++ src/main/java/de/gie/tool/urltool/UrlToolApplication.java
@@ -0,0 +1,13 @@
+package de.gie.tool.urltool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/gie/tool/urltool/UrlToolApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gie.tool.urltool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
